bbe57a48a9a700776768be0a9125d7f9f2b5cca4
arktony <arktumwesigye@gmail.com>
2020-12-13 09:40:06 +0300

    Major Shell make up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+import { HomeComponent } from './home/home.component';
+import { TopperComponent } from './topper/topper.component';
+import { SidewaysComponent } from './sideways/sideways.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    HomeComponent,
+    TopperComponent,
+    SidewaysComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: LandingComponent },
+      { path: 'home', component: HomeComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sideways/sideways.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sideways',
+  templateUrl: './sideways.component.html',
+  styleUrls: ['./sideways.component.css']
+})
+export class SidewaysComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
